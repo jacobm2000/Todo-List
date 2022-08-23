78cca5a329b19f099bb99e7bb9166c03b2f02486
jacobm2000 <maurerja2000@gmail.com>
2022-08-22 21:33:00 -0400

    updated functionality
    Made the program function by allowing adding of tasks, editing, and removing

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
 var l=document.getElementById('t')
 var button=document.getElementById('add-btn')
 var textbox=document.getElementById('textbox')
+var edit=document.getElementById('edit')
+var editBtn=document.getElementById('editBtn')
+var tasks=[]
 
+function update() {
+    l.innerHTML=""
+var len=tasks.length
+for(let i=0; i<len;i++){
+    var btnDel=document.createElement('button')
+    var btnEdit=document.createElement('button')
+    btnEdit.appendChild(document.createTextNode('edit'))
+    btnEdit.classList.add("btn-warning")
+    btnEdit.classList.add("float-right")
+    
+    
+    
+    btnDel.appendChild(document.createTextNode('remove'))
+    btnDel.classList.add("btn-danger")
+    btnDel.classList.add("float-right")
+    btnDel.addEventListener("click", function(){
+        tasks.splice(tasks[i],1)
+    update()
+    }
+    )
+    btnEdit.addEventListener("click", function(){
+        document.getElementById('editbox').value=tasks[i]
+        document.getElementById('edit').style.display="flex"
+        editBtn.onclick=function(){
+            tasks[i]=document.getElementById('editbox').value
+            document.getElementById('edit').style.display="none"
+            update()
+        }    
+    
+    }
+    )
+    var node = document.createElement('li')
+    node.appendChild(document.createTextNode(tasks[i]))
+    
+    node.classList.add("list-group-item")
+    node.classList.add("w-25")
+    node.append(btnEdit)
+    node.append(btnDel)
+    
+    l.appendChild(node)
+    
+}
+}
 
 
 button.addEventListener('click', add)
 function add()
 {
-var btn=document.createElement('button')
-btn.appendChild(document.createTextNode('remove'))
-btn.classList.add("btn-primary")
-btn.classList.add("float-right")
-btn.addEventListener("click", function(){btn.parentElement.remove()})
-text=textbox.value
-var node = document.createElement('li')
-node.appendChild(document.createTextNode(text))
-node.classList.add("list-group-item")
-node.classList.add("w-25")
-node.append(btn)
- 
-l.appendChild(node)
-
-
+tasks.push(textbox.value)
+update()
 }
 
 
